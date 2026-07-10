413d88da3c0ea76fa39a441b52fdd53825b86aef
TimAaronSharp <timothyaaron.sharp@gmail.com>
2026-07-09 18:30:26 -0600

    Tutorial complete. Beginning refactor. Created FilterableProductTable.tsx, ProductTable.tsx, and SearchBar.tsx for individual component files.

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,14 @@
+
+
+function SearchBar({ filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange }: SearchBarProps) {
+  return (
+    <form>
+      <input type="text" value={filterText} placeholder="Search..." onChange={(e) => onFilterTextChange(e.target.value)} />
+      <label>
+        <input type="checkbox" onChange={(e) => onInStockOnlyChange(e.target.checked)} />
+        {' '}
+        Only show products in stock
+      </label>
+    </form>
+  );
+}
